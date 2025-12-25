abc40ca40cea7b8e59c4aa23998742f725e85ea8
Ori Gerstein <ori.gerstein@post.runi.ac.il>
2025-12-25 14:41:56 +0200

    completed

--- Binomial.java
+++ Binomial.java
@@ -6,14 +6,14 @@ public class Binomial {
 		// Testing the basic binomial implementation:
     	System.out.println(binomial(Integer.parseInt(args[0]),Integer.parseInt(args[1])));
 		// Testing the optimized binomial implementation:
-		// System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+		System.out.println(binomial(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	}
 
-// 	// Computes the Binomial function, basic version.
-// 	public static int binomial1(int n, int k) { 
-// 	if (k > n) return 0;
-// 	if (k == 0 || n == 0) return 1;
-// 	return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
+	// Computes the Binomial function, basic version.
+	public static int binomial1(int n, int k) { 
+	if (k > n) return 0;
+	if (k == 0 || n == 0) return 1;
+return binomial1(n - 1, k) + binomial1(n - 1, k - 1);}
 	
 // }
 	
